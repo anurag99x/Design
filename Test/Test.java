b225d77d0c1ea2f4906dbfce86020b5de908cf19
anurag99x <anuragxpandey@gmail.com>
2025-03-15 11:03:54 +0530

    Revert "commit#1"
    This reverts commit 2f2dc14e5e56af9b9d2df18d2fe6fda824b838b6.

--- Test/Test.java
+++
@@ -1,8 +0,0 @@
-package DesignProblems.commandLineTool.Test;
-
-public class Test {
-    public static void main(String[] args) {
-        boolean[] visited = new boolean[10];
-        depthFirstSearch();
-    }
-}
